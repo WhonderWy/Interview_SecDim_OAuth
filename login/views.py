c2ab0ffbff18cc3da6a62561fa45e5828f077085
z5017279 <26218326+WhonderWy@users.noreply.github.com>
2022-02-01 18:48:37 +1100

    Fixed threading in django!

--- login/views.py
+++ login/views.py
@@ -37,8 +37,8 @@ def update_state():
         time.sleep(60 * 10)
 
 
-# state_update = threading.Thread(target=update_state, daemon=True)
-# state_update.run()
+state_update = threading.Thread(target=update_state, daemon=True)
+state_update.start()
 
 
 def handle_oauth() -> str:
@@ -86,7 +86,6 @@ class LoginAction(views.APIView):
     def get(self, request):
         try:
             dotenv.read_dotenv()
-            # state_update.run()
             result = ""
             parameters = {
                 "client_id": os.getenv("SECRET_CLIENT_ID_FOR_NOW"),
@@ -123,34 +122,31 @@ class Continue(views.APIView):
             #     print("it works this time though?")
             if request.GET["state"] == state and request.GET["code"]:
                 github_code: str = request.GET["code"]
-                header = {
-                    "Accept": "application/json"
-                }
+                header = {"Accept": "application/json"}
                 post_body = {
                     "client_id": os.getenv("SECRET_CLIENT_ID_FOR_NOW"),
                     "client_secret": os.getenv("SECRET_CLIENT_SECRET"),
                     "code": github_code,
                     "redirect_uri": "http://localhost:8000/continue/",
                 }
-                post_response = session.post(ACCESS_TOKEN_URL, data=post_body, headers=header)
+                post_response = session.post(
+                    ACCESS_TOKEN_URL, data=post_body, headers=header
+                )
                 access_token = post_response.json()["access_token"]
-                # print(access_token)
-                # time.sleep(1)
-                # header = {
-                #     "Authorization": f"token {access_token}",
-                #     "Accept": "application/vnd.github.v3+json",
-                # }
-                # email_response = session.get(USER_API_URL, headers=header)
-                # if email_response.ok and email_response.json()["email"]:
-                #     result = email_response.json()["email"]
-                #     print(result)
-                # else:
-                #     time.sleep(1)
-                #     email_response = session.get(EMAIL_API_URL, headers=header)
-                #     result = email_response.json()
-                #     print(result)
-                # token = post_response.json()["access_token"]
-            return HttpResponsePermanentRedirect(f"http://localhost:3000/?pointlessToken={access_token}")
+                return HttpResponsePermanentRedirect(
+                    f"http://localhost:3000/?pointlessToken={access_token}"
+                )
+            elif request.GET["error"]:
+                return HttpResponseBadRequest(
+                    f"""
+                    <h1>{request.GET["error"] if request.GET["error"] else "Error"}</h1>
+                    <p>{request.GET["error_description"] if request.GET["error_description"] else "Something happened"}</p>
+                    <p>Please check the below for more information.</p>
+                    <a src="{request.GET["error_uri"] if request.GET["error_uri"] else "https://google.com.au/"}">Link here</a>
+                """
+                )
+            else:
+                raise Exception
         except:
             return HttpResponseBadRequest("Did something wrong.")
 
@@ -172,7 +168,9 @@ class Continue(views.APIView):
                     "Authorization": "token " + access_token,
                     "Accept": "application/vnd.github.v3+json",
                 }
-            return JsonResponse({"pointlessToken": token, "emails": result})
+                return JsonResponse({"pointlessToken": token, "emails": result})
+            else:
+                raise Exception
         except:
             return HttpResponseBadRequest("Did something wrong.")
 
@@ -197,7 +195,9 @@ class LoggedIn(views.APIView):
                 email_response = session.get(USER_API_URL, headers=header)
                 if email_response.ok and email_response.json()["email"]:
                     result = email_response.json()["email"]
-            return JsonResponse({"email": result})
+                return JsonResponse({"email": result})
+            else:
+                raise Exception
         except:
             return HttpResponseBadRequest("Did something wrong.")
 
@@ -246,6 +246,7 @@ class GetEmail(views.APIView):
 
     def post(self, request):
         return HttpResponse("Test")
+
 
 # Create your views here.
 def index(request):
